2990ff16d6fce96f9ab36ddc91fc093b28a450a4
srafin <srafin@2992784e-be46-0410-bb3d-b9c29436a3b8>
2008-09-14 19:42:40 +0000

    Use automatically generated create_symlinks.sh script
    git-svn-id: https://svn.code.sf.net/p/tomplayer/svn/trunk@164 2992784e-be46-0410-bb3d-b9c29436a3b8

--- distrib/tomplayer/tomplayergui.sh
+++ distrib/tomplayer/tomplayergui.sh
@@ -37,73 +37,19 @@ export TSLIB_CONSOLEDEVICE=none
 export TSLIB_FBDEVICE=/dev/fb
 export TSLIB_TSDEVICE=/dev/ts
 export TSLIB_CONFFILE=/etc/ts.conf
-export TSLIB_PLUGINDIR=/usr/lib/ts
+export TSLIB_PLUGINDIR=/usr/local/lib/ts
 
 
 export LIB_TOMPLAYER=${TOMPLAYER_DIR}/lib
 export CONF_TOMPLAYER=${TOMPLAYER_DIR}/conf
+
+
+# Call script that creates symlinks for shared ibrairies
+./create_symlinks.sh
+
+# Copy tslib configuration file
 cd /etc
 ln -sf ${CONF_TOMPLAYER}/ts.conf ts.conf
-
-mkdir -p /usr/lib
-cd /usr/lib
-ln -sf ${LIB_TOMPLAYER}/pkgconfig pkgconfig
-ln -sf ${LIB_TOMPLAYER}/directfb-1.2-0 directfb-1.2-0
-ln -sf ${LIB_TOMPLAYER}/ts ts
-ln -sf ${LIB_TOMPLAYER}/libfusion-1.2.so.0.0.0 libfusion-1.2.so.0.0.0
-ln -sf ${LIB_TOMPLAYER}/libILUT.la libILUT.la
-ln -sf ${LIB_TOMPLAYER}/libts-0.0.so.0.1.1 libts-0.0.so.0.1.1
-ln -sf ${LIB_TOMPLAYER}/libfreetype.so.6.3.16 libfreetype.so.6.3.16
-ln -sf ${LIB_TOMPLAYER}/libzip.la libzip.la
-ln -sf ${LIB_TOMPLAYER}/libIL.la libIL.la
-ln -sf ${LIB_TOMPLAYER}/libILU.la libILU.la
-ln -sf ${LIB_TOMPLAYER}/libdirect.la libdirect.la
-ln -sf ${LIB_TOMPLAYER}/libiniparser.so.0 libiniparser.so.0
-ln -sf ${LIB_TOMPLAYER}/libdirect-1.2.so.0.0.0 libdirect-1.2.so.0.0.0
-ln -sf ${LIB_TOMPLAYER}/libIL.so.1.0.0 libIL.so.1.0.0
-ln -sf ${LIB_TOMPLAYER}/libjpeg.la libjpeg.la
-ln -sf ${LIB_TOMPLAYER}/libfusion.la libfusion.la
-ln -sf ${LIB_TOMPLAYER}/libILUT.so.1.0.0 libILUT.so.1.0.0
-ln -sf ${LIB_TOMPLAYER}/libdirectfb.la libdirectfb.la
-ln -sf ${LIB_TOMPLAYER}/libz.so.1.2.3 libz.so.1.2.3
-ln -sf ${LIB_TOMPLAYER}/libpng12.so.0.29.0 libpng12.so.0.29.0
-ln -sf ${LIB_TOMPLAYER}/libILU.so.1.0.0 libILU.so.1.0.0
-ln -sf ${LIB_TOMPLAYER}/libpng12.la libpng12.la
-ln -sf ${LIB_TOMPLAYER}/libts.la libts.la
-ln -sf ${LIB_TOMPLAYER}/libjpeg.so.62.0.0 libjpeg.so.62.0.0
-ln -sf ${LIB_TOMPLAYER}/libzip.so.1.0.0 libzip.so.1.0.0
-ln -sf ${LIB_TOMPLAYER}/libpng.so.3.29.0 libpng.so.3.29.0
-ln -sf ${LIB_TOMPLAYER}/libdirectfb-1.2.so.0.0.0 libdirectfb-1.2.so.0.0.0
-ln -sf ${LIB_TOMPLAYER}/libfreetype.la libfreetype.la
-ln -sf ${LIB_TOMPLAYER}/libts-0.0.so.0.1.1 libts.so
-ln -sf ${LIB_TOMPLAYER}/libz.so.1.2.3 libz.so.1
-ln -sf ${LIB_TOMPLAYER}/libIL.so.1.0.0 libIL.so
-ln -sf ${LIB_TOMPLAYER}/libpng12.la libpng.la
-ln -sf ${LIB_TOMPLAYER}/libpng12.so.0.29.0 libpng12.so
-ln -sf ${LIB_TOMPLAYER}/libfreetype.so.6.3.16 libfreetype.so
-ln -sf ${LIB_TOMPLAYER}/libz.so.1.2.3 libz.so
-ln -sf ${LIB_TOMPLAYER}/libpng.so.3.29.0 libpng.so.3
-ln -sf ${LIB_TOMPLAYER}/libfusion-1.2.so.0.0.0 libfusion-1.2.so.0
-ln -sf ${LIB_TOMPLAYER}/libdirect-1.2.so.0.0.0 libdirect-1.2.so.0
-ln -sf ${LIB_TOMPLAYER}/libpng12.pc libpng.pc
-ln -sf ${LIB_TOMPLAYER}/libILUT.so.1.0.0 libILUT.so
-ln -sf ${LIB_TOMPLAYER}/libdirect-1.2.so.0.0.0 libdirect.so
-ln -sf ${LIB_TOMPLAYER}/libIL.so.1.0.0 libIL.so.1
-ln -sf ${LIB_TOMPLAYER}/libILU.so.1.0.0 libILU.so
-ln -sf ${LIB_TOMPLAYER}/libILU.so.1.0.0 libILU.so.1
-ln -sf ${LIB_TOMPLAYER}/libdirectfb-1.2.so.0.0.0 libdirectfb.so
-ln -sf ${LIB_TOMPLAYER}/libiniparser.so.0 libiniparser.so
-ln -sf ${LIB_TOMPLAYER}/libfreetype.so.6.3.16 libfreetype.so.6
-ln -sf ${LIB_TOMPLAYER}/libjpeg.so.62.0.0 libjpeg.so
-ln -sf ${LIB_TOMPLAYER}/libfusion-1.2.so.0.0.0 libfusion.so
-ln -sf ${LIB_TOMPLAYER}/libzip.so.1.0.0 libzip.so
-ln -sf ${LIB_TOMPLAYER}/libts-0.0.so.0.1.1 libts-0.0.so.0
-ln -sf ${LIB_TOMPLAYER}/libzip.so.1.0.0 libzip.so.1
-ln -sf ${LIB_TOMPLAYER}/libdirectfb-1.2.so.0.0.0 libdirectfb-1.2.so.0
-ln -sf ${LIB_TOMPLAYER}/libpng12.so libpng.so
-ln -sf ${LIB_TOMPLAYER}/libjpeg.so.62.0.0 libjpeg.so.62
-ln -sf ${LIB_TOMPLAYER}/libpng12.so.0.29.0 libpng12.so.0
-ln -sf ${LIB_TOMPLAYER}/libILUT.so.1.0.0 libILUT.so.1
 
 
 cd $TOMPLAYER_DIR
