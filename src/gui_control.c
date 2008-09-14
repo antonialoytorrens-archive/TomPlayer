86995a48bc4f2088ca41f5d3a7be0099254342c1
srafin <srafin@2992784e-be46-0410-bb3d-b9c29436a3b8>
2008-09-14 19:13:56 +0000

    git-svn-id: https://svn.code.sf.net/p/tomplayer/svn/trunk@157 2992784e-be46-0410-bb3d-b9c29436a3b8

--- src/gui_control.c
+++ src/gui_control.c
@@ -20,7 +20,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#include <directfb.h>
+#include "directfb/directfb.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "gui.h"
