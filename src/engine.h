e9257f1d22e11d70ad6fa6e5b74a7806fe48c337
srafin <srafin@2992784e-be46-0410-bb3d-b9c29436a3b8>
2008-11-08 03:03:43 +0000

    litlle cleanup in exported functions and variables
    git-svn-id: https://svn.code.sf.net/p/tomplayer/svn/trunk@220 2992784e-be46-0410-bb3d-b9c29436a3b8

--- src/engine.h
+++ src/engine.h
@@ -28,24 +28,10 @@
 
 
 extern struct tomplayer_config config;
-extern bool is_menu_showed;
 extern bool is_mplayer_finished;
-extern bool is_playing_video;
-extern bool is_playing_audio;
 
 int init_engine( void );
 int release_engine( void );
-void generate_thumbnail( void );
-void get_thumbnail_name( char * folder, char * file, char * thumb );
-bool file_exist( char * file );
-
-char * get_file_extension( char * file );
-bool is_skin_file( char * file );
-bool is_video_file( char * file );
-bool is_audio_file( char * file );
-bool has_extension( char * file, char * extensions );
-
-void display_current_file( char * filename, struct skin_config *skin_conf, ILuint bitmap );
 
 void send_command(const char * cmd );
 void send_menu( char * cmd );
@@ -53,22 +39,9 @@ void send_menu( char * cmd );
 void show_menu( void );
 void hide_menu( void );
 
-/*void blit_video_menu( int fifo, struct skin_config * conf );*/
-
-void * mplayer_thread(void *cmd);
-void launch_mplayer( char * folder, char * filename, int pos );
-
-
 void handle_mouse_event( int x, int y );
 int get_command_from_xy( int x, int y, int * p );
-
-
 void display_image_to_fb( ILuint  );
 
-/* This function is GUI library dependent, it's not defined in engine.c but in gui.c */
-/*void display_current_file( char * , struct skin_config *, ILuint );*/
-
-
-void gui_buffer_rgb(char * buffer,int width, int height, int x, int y);
 
 #endif /* __ENGINE_H__ */
