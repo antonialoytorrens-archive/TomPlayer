0964e165fa5514c4c67495bc62f59b9aa2fef9eb
srafin <srafin@2992784e-be46-0410-bb3d-b9c29436a3b8>
2008-11-16 02:15:53 +0000

    Modifs for v0.212
    git-svn-id: https://svn.code.sf.net/p/tomplayer/svn/trunk@268 2992784e-be46-0410-bb3d-b9c29436a3b8

--- distrib/tomplayer/tomplayergui.sh
+++ distrib/tomplayer/tomplayergui.sh
@@ -6,18 +6,17 @@ cd $TOMPLAYER_DIR
 
 #To be sure to get absolute path
 TOMPLAYER_DIR=`pwd`
+export LIB_TOMPLAYER=${TOMPLAYER_DIR}/lib
+export CONF_TOMPLAYER=${TOMPLAYER_DIR}/conf
+
 
 echo "Killing ttn..."
-
 killall -9 mplayer
 killall -9 ttn
 killall -9 mp3d 
 killall -9 a2dpd
-
 rm /mnt/sdcard/dirty_fs 
-
 /etc/rc.usbkill 
-
 killall -9 clmapp
 killall -9 ttsserver 
 killall -9 suntime 
@@ -26,40 +25,61 @@ killall -9 suntime
 killall -9 refresh_wdg 
 ./refresh_wdg &
 
+# Call script that creates symlinks for shared librairies
+./create_symlinks.sh
+
 #convert in UNIX text format the used configuration file
 cp -f conf/tomplayer.ini /tmp/tomplayer.ini
 dos2unix /tmp/tomplayer.ini
 
-#!/bin/sh
+# Test whether /dev/ts is handled by a driver or not
+echo -n "" >> /dev/ts
+RES=$?
+if [ $RES -ne 0 ] ; then
+NO_TS=1
+else
+NO_TS=0
+fi 
+
+
+# Useful env vars for DirectFB and Tslib
 export FRAMEBUFFER=/dev/fb
 export TSLIB_CONSOLEDEVICE=none
 export TSLIB_FBDEVICE=/dev/fb
-export TSLIB_TSDEVICE=/dev/ts
 export TSLIB_CONFFILE=/etc/ts.conf
 export TSLIB_PLUGINDIR=/usr/local/lib/ts
-
-export LIB_TOMPLAYER=${TOMPLAYER_DIR}/lib
-export CONF_TOMPLAYER=${TOMPLAYER_DIR}/conf
-
 export LD_LIBRARY_PATH=/usr/local/lib
 
-# Call script that creates symlinks for shared librairies
-./create_symlinks.sh
 
-# Copy tslib configuration file
-cd /etc
-ln -sf ${CONF_TOMPLAYER}/ts.conf ts.conf
+# Adapt to whether /dev/ts or /dev/input/event0 as touchscreen inputs
+if [ $NO_TS -eq 0 ] ; then 
+  export TSLIB_TSDEVICE=/dev/ts
+  # Copy tslib configuration file
+  cd /etc
+  ln -sf ${CONF_TOMPLAYER}/ts.conf ts.conf
+  cd $TOMPLAYER_DIR
+else
+  # For Linux inputs device we need to calibrate !
+  export TSLIB_TSDEVICE=/dev/input/event0
+  export TSLIB_CALIBFILE=${CONF_TOMPLAYER}/pointercal
+  # Copy tslib configuration file
+  cd /etc
+  ln -sf ${CONF_TOMPLAYER}/ts_input.conf ts.conf
+  cd $TOMPLAYER_DIR
+  if [ ! -f ${CONF_TOMPLAYER}/pointercal ] ; then
+    ./ts_calibrate
+  fi
+fi
 
 
-cd $TOMPLAYER_DIR
-
+# Tomplayer / start_engine loop 
 END_ASKED=0
 NO_SPLASH=
 while [  $END_ASKED -eq 0 ]; do 
 rm /tmp/start_engine.sh
 ./tomplayer --dfb:no-vt $NO_SPLASH
 sync
-./splash_screen res/background/loading
+./splash_screen res/background/wait
 if [ -f /tmp/start_engine.sh ] ; then 
   /bin/sh /tmp/start_engine.sh
    NO_SPLASH="--no-splash"
@@ -68,13 +88,10 @@ else
 fi
 done
 
+# Launch navigation software
 ttn_file=/bin/ttn
-
 ttn_cnt=`ps | grep ttn | wc -l`
-
 if test ${ttn_cnt} -lt 2 ; then 
-
-
 echo "Start TTN"
 /etc/rc.restartgps
 ${ttn_file}
