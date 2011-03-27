8cfb0980b57b4836e78d788529243e7f47e90128
srafin <srafin@2992784e-be46-0410-bb3d-b9c29436a3b8>
2011-03-27 18:49:05 +0000

    Explicit funtion prototye declaration
    git-svn-id: https://svn.code.sf.net/p/tomplayer/svn/trunk@395 2992784e-be46-0410-bb3d-b9c29436a3b8

---
+++ src/event_inputs.h
@@ -0,0 +1,32 @@
+/**
+ * \file event_inputs.h
+ * \author Wolfgar 
+ * \brief This module deals with inputs whatever the are (key posted in FIFO or event from touchscreen)
+ * 
+ * $URL$
+ * $Rev$
+ * $Author$
+ * $Date$
+ *
+ */
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */ 
+#ifndef __EVENT_INPUTS_H__
+#define __EVENT_INPUTS_H__
+void event_loop(void);
+#endif
+
